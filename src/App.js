28cdcd4b6605d36f8594a01e12343ef1c12c9b20
Samuel Macedo <samuel.macfer@gmail.com>
2021-05-26 19:52:54 -0300

    ci: add fetch featuredMovie

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
 import React, { useEffect, useState } from 'react';
 
 import { MovieRow } from './components/MovieRow';
+import { FeaturedMovie } from './components/FeaturedMovie.js';
 
-import { getHomeList } from './services/api.js';
+import { getHomeList, getMovieInfo } from './services/api.js';
 
 import './App.css';
 
 export default function App() {
 	const [movieList, setMovieList] = useState([]);
+	const [featuredData, setFeaturedData] = useState(null);
 
 	useEffect(() => {
 		const loadAll = async () => {
 			//Get all the list
 			let list = await getHomeList();
 			setMovieList(list);
+
+			//Get Feature data
+			let originals = list.filter((item) => item.slug === 'originals');
+			let randomChosen = Math.floor(
+				Math.random() * originals[0].items.results.length - 1
+			);
+			let chosen = originals[0].items.results[randomChosen];
+			let chosenInfo = await getMovieInfo(chosen.id, 'tv');
+			setFeaturedData(chosenInfo);
+			console.log(chosenInfo);
 		};
 
 		loadAll();
@@ -21,6 +33,8 @@ export default function App() {
 
 	return (
 		<div className='page'>
+			{featuredData && <FeaturedMovie item={featuredData} />}
+
 			<section className='lists'>
 				{movieList.map((item, key) => (
 					<MovieRow key={key} title={item.title} items={item.items} />
---
+++ src/components/FeaturedMovie.js/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './styles.css';
+
+export function FeaturedMovie() {
+	return (
+		<section className='featured'>
+			<div>EM DESTAQUE</div>
+		</section>
+	);
+}
