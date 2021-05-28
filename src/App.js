7922b6eee42f1e0cda04b4cead9bbd49d50dc1b8
Samuel Macedo <samuel.macfer@gmail.com>
2021-05-28 19:43:53 -0300

    ci: add Footer component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react';
 import { MovieRow } from './components/MovieRow';
 import { FeaturedMovie } from './components/FeaturedMovie';
 import { Header } from './components/Header';
+import { Footer } from './components/Footer';
 
 import { getHomeList, getMovieInfo } from './services/api.js';
 
@@ -58,6 +59,8 @@ export default function App() {
 					<MovieRow key={key} title={item.title} items={item.items} />
 				))}
 			</section>
+
+			<Footer />
 		</div>
 	);
 }
